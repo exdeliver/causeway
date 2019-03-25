a0a268c71388875c067b1571a7fa9d7bcdd5b0e9
jasonhoendervanger <hoendervanger@eur.nl>
2019-03-25 17:57:54 +0100

    initial commit

--- src/Views/layouts/partials/_breadcrumbs.blade.php
+++ src/Views/layouts/partials/_breadcrumbs.blade.php
@@ -3,7 +3,7 @@
         <div class="col-md-12">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
                     @yield('breadcrumbs')
                 </ol>
             </nav>
