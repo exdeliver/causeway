6de8f5167d04a796e184752d8ec0109b50c0b912
jasonhoendervanger <hoendervanger@eur.nl>
2019-03-28 19:40:35 +0100

    initial commit

--- database/migrations/2019_02_15_172030_seed_permissions_to_permissions_table.php
+++ database/migrations/2019_02_15_172030_seed_permissions_to_permissions_table.php
@@ -11,19 +11,24 @@ class SeedPermissionsToPermissionsTable extends Migration
      */
     public function up()
     {
-        \Spatie\Permission\Models\Role::create([
-            'name' => 'admin',
-            'guard_name' => 'web',
-        ]);
+        if(\Spatie\Permission\Models\Role::where('name', 'user')->first() === null) {
+            \Spatie\Permission\Models\Role::create([
+                'name' => 'admin',
+                'guard_name' => 'web',
+            ]);
+        }
+        if(\Spatie\Permission\Models\Role::where('name', 'user')->first() === null) {
+            \Spatie\Permission\Models\Role::create([
+                'name' => 'user',
+                'guard_name' => 'web',
+            ]);
+        }
 
-        \Spatie\Permission\Models\Role::create([
-            'name' => 'user',
-            'guard_name' => 'web',
-        ]);
-
-        $permission = \Spatie\Permission\Models\Permission::create([
-            'name' => 'manage.group',
-        ]);
+        if(\Spatie\Permission\Models\Permission::where('name', 'manage.group')->first() === null) {
+            $permission = \Spatie\Permission\Models\Permission::create([
+                'name' => 'manage.group',
+            ]);
+        }
 
         $role = \Spatie\Permission\Models\Role::where('name', 'user')->first();
 
