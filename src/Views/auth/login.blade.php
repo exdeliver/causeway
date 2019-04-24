85f27f6bbae0a62ab5fd0cf9ff58e9c8e05b6cc9
jasonhoendervanger <hoendervanger@eur.nl>
2019-04-24 14:39:24 +0200

    Update requirements

--- src/Views/auth/login.blade.php
+++ src/Views/auth/login.blade.php
@@ -8,6 +8,10 @@
                     <div class="card-header">{{ __('Login') }}</div>
 
                     <div class="card-body">
+                        <p class="text-center" style="padding-bottom: 0px; margin-bottom: 0px;">
+                            <small><a href="http://www.exdeliver.nl" target="_blank">EXdeliver</a> presents...</small>
+                        </p>
+                        <h1 class="text-center" style="padding-top: 0px; margin-top: 0px;">Causeway CMS</h1>
                         @include('causeway::layouts.partials._status_messages')
                         <login-component
                                 csrf_token="{{ csrf_token() }}"
