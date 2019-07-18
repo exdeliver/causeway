bdbba25d61237bdb37a429e3def7c65c3c5cc228
jasonhoendervanger <hoendervanger@eur.nl>
2019-07-18 20:44:09 +0200

    active state in menu

--- src/Views/site/menu/_default_item.blade.php
+++ src/Views/site/menu/_default_item.blade.php
@@ -1,5 +1,5 @@
 @if(count($item->items) > 0)
-    <li class="nav-item dropdown">
+    <li class="nav-item dropdown {{ request()->is($item->url .'*') ? 'active' : '' }}">
         <a href="{{ $item->url }}" class="nav-link dropdown-toggle" id="navbar{{ str_slug($item->label) }}" role="button" data-toggle="dropdown" aria-haspopup="true"
            aria-expanded="false">{{ $item->label }}</a>
         <div class="dropdown-menu" aria-labelledby="navbar{{ str_slug($item->label) }}">
@@ -12,7 +12,7 @@
     @if($item->isSubmenu())
         <a class="dropdown-item" href="{{ $item->url }}">{{ $item->label }}</a>
     @else
-        <li class="nav-item">
+        <li class="nav-item {{ request()->is($item->url .'*') ? 'active' : '' }}">
             <a href="{{ $item->url }}" class="nav-link">{{ $item->label }}</a>
         </li>
     @endif
