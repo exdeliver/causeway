188d694a2343fc8abc3d675f37abaf42c586af11
jasonhoendervanger <hoendervanger@eur.nl>
2019-08-08 16:04:17 +0200

    Made query mysql 5.7 compatible. groupby

--- src/Domain/Services/ShopProductService.php
+++ src/Domain/Services/ShopProductService.php
@@ -62,7 +62,24 @@ final class ShopProductService extends AbstractService
             '*',
             DB::raw('IF(shop_products.special_price IS NOT NULL AND shop_products.special_price < shop_products.gross_price, shop_products.special_price, shop_products.gross_price) * ((vat/100)+1) as price')
         )
-            ->groupBy(['shop_products.id'])
+            ->groupBy([
+                'shop_products.id',
+                'shop_products.parent_product_id',
+                'shop_products.title',
+                'shop_products.slug',
+                'shop_products.type',
+                'shop_products.active',
+                'shop_products.description',
+                'shop_products.quantity',
+                'shop_products.gross_price',
+                'shop_products.special_price',
+                'shop_products.vat',
+                'shop_products.pid',
+                'shop_products.weight',
+                'shop_products.sequence',
+                'shop_products.created_at',
+                'shop_products.updated_at',
+            ])
             ->whereIn('id', $shopCategory->products->pluck('product_id')->toArray());
 
         return $products;
